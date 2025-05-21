c736a259f04ff3e8baf414b6148ff9ad2c73b1b2
MalakBo <malakbouyi8@gmail.com>
2025-05-21 21:04:07 +0100

    first commit

---
+++ philosophers.h
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mac <mac@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 16:30:25 by mbouyi            #+#    #+#             */
+/*   Updated: 2025/05/05 17:14:24 by mac              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+#define PHILOSOPHERS_H
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <limits.h>
+
+typedef struct s_fork
+{
+    pthread_mutex_t fork;
+    int id_fork;
+    
+} t_fork;
+
+typedef struct s_philo
+{
+    pthread_t thread;
+    int     id;
+    int meals_eaten;
+    long last_meal_time;
+    t_fork *left;
+    t_fork *right;
+    t_data *data;
+} t_philo;
+
+typedef struct s_data
+{
+    int philos_number;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int stop_flag;
+    time_t start_sim;
+    time_t end_sim;
+    t_fork *forks;
+    t_philo *philos;
+} t_data;
+#endif
