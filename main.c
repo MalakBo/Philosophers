4264ccab633db9b6f2e8c8410d7856036171102d
MalakBo <malakbouyi8@gmail.com>
2025-05-22 19:03:00 +0100

    w

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mbouyi <mbouyi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 15:36:15 by mbouyi            #+#    #+#             */
-/*   Updated: 2025/05/22 16:23:51 by mbouyi           ###   ########.fr       */
+/*   Updated: 2025/05/22 18:51:49 by mbouyi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,10 @@ long ft_atoi(char *s)
     }
     return (r * sign);
 }
+int init_data(t_data *data,int argc,char **argv)
+{
+    
+}
 int is_digit(char *n)
 {
     int i = 0;
@@ -58,17 +62,18 @@ int main(int argc, char **argv)
         {
             if (!is_digit(argv[i]))
             {
-                write(2, "dkhli ghi arqam akhoya\n", 24);
+                write(2, "dkhli ghi arqam a ikhan\n", 24);
                 return 1;
             }
             i++;
         }
-        write(1, "hakak\n", 6);
+        write(1, "hakak a dk lmongol\n", 19);
     }
     else 
     {
-        write(2, "zid shwiya wla nqes\n", 21);
+        write(2, "zid shwiya wla <nqes a dk lmo3a9\n", 32);
         return 1;
     }
+    
     return 0;
 }
